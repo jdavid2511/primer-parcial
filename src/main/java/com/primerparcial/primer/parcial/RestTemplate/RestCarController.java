871d5943431331a92d98a2471e4d4b7113251bb1
jdavid2511 <jdgarciat@ufpso.edu.co>
2023-04-14 21:02:56 -0500

    realizar consulta mediante RestTemplate para visualizar por id y visualizar todoa

---
+++ src/main/java/com/primerparcial/primer/parcial/RestTemplate/RestCarController.java
@@ -0,0 +1,32 @@
+package com.primerparcial.primer.parcial.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.primerparcial.primer.parcial.dto.CarDTO;
+import com.primerparcial.primer.parcial.service.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "api/cars/")
+public class RestCarController {
+
+    private final RestCarService restCarService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity getCar(@PathVariable Long id) throws JsonProcessingException {
+        return new ResponseEntity(restCarService.getById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<CarDTO>> getAllCars() throws JsonProcessingException {
+        return new ResponseEntity(restCarService.getAllCars(), HttpStatus.OK);
+    }
+}
